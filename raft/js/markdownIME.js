d52344ce537a8a9de75eaf2875da0ec2351fb1e9
imTim12138 <hjltim@icloud.com>
2016-08-05 21:38:23 +0800

    删除了错误的多余目录

--- raft/js/markdownIME.js
+++
@@ -1,5 +0,0 @@
-var editor = document.getElementById('editor');
-MarkdownIME.Enhance(editor);
-
-//optional: enable Tex Formula support
-MarkdownIME.Renderer.inlineRenderer.addRule(new MarkdownIME.Addon.MathAddon())
